7350e67d309d499eb235309c60ef232f6e9f9489
calilaceron <calil.aceron@student.ateneo.edu>
2026-03-02 23:35:04 +0800

    Modifies dynamic url for recipe list

--- ledger/urls.py
+++ ledger/urls.py
@@ -4,7 +4,7 @@ from .views import LedgerListView, LedgerDetailView
 
 urlpatterns = [
     path('recipes/list', LedgerListView.as_view(), name='recipe-list'),
-    path('recipes/<int:pk>', LedgerDetailView.as_view(), name="recipe-detail"),
+    path('recipe/<int:pk>', LedgerDetailView.as_view(), name="recipe-detail"),
 ]
 
 app_name = "ledger"
