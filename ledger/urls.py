c40eb0af58f12f151f431f33d7b246541de185fb
calilaceron <calil.aceron@student.ateneo.edu>
2026-03-12 20:56:37 +0800

    Update urls for add recipe and image views

--- ledger/urls.py
+++ ledger/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
 
-from .views import LedgerListView, LedgerDetailView
+from .views import (
+    LedgerListView,
+    LedgerDetailView,
+    LedgerCreateView,
+    ImageUploadView
+)
 
 urlpatterns = [
+    path('recipe/<int:pk>/add_image', ImageUploadView.as_view(), name='image-add'),
+    path('recipe/add', LedgerCreateView.as_view(), name='recipe-add'),
     path('recipes/list', LedgerListView.as_view(), name='recipe-list'),
-    path('recipe/<int:pk>', LedgerDetailView.as_view(), name="recipe-detail"),
+    path('recipe/<int:pk>', LedgerDetailView.as_view(), name='recipe-detail'),
 ]
 
-app_name = "ledger"
+app_name = 'ledger'
