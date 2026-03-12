75825d696962e013b1491923bb935c6d25a99459
calilaceron <calil.aceron@student.ateneo.edu>
2026-03-12 20:51:35 +0800

    adds Recipe Image model to Recipe Admin

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,13 +1,20 @@
 from django.contrib import admin
-from .models import Recipe, RecipeIngredient, Ingredient
+from .models import Recipe, RecipeIngredient, Ingredient, RecipeImage
 
 
 class RecipeIngredientInline(admin.TabularInline):
     model = RecipeIngredient
 
 
+class RecipeImageInline(admin.TabularInline):
+    model = RecipeImage
+
+
 class RecipeAdmin(admin.ModelAdmin):
-    inlines = [RecipeIngredientInline,]
+    inlines = [
+        RecipeIngredientInline,
+        RecipeImageInline
+    ]
     list_display = ("name", "author", "created_on", "updated_on")
     readonly_fields = ("created_on", "updated_on")
     fields = ("name", "author", "created_on", "updated_on")
